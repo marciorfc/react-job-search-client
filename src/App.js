5cc9172b4418084859ebba5edceced3568404e10
marciorc <mroberto@gmail.com>
2020-04-02 23:49:57 -0300

    jobList state

--- src/App.js
+++ src/App.js
@@ -11,28 +11,30 @@ const mockJobs = [
   {title: 'SWE 3', company: 'Apple'},
 ];
 
-async function fetchJobs() {
+async function fetchJobs(updateCb) {
   const res = await fetch(JOB_API_URL);
   const json = await res.json();
+
+  updateCb(json);
   console.log({json});
 }
 
 
 function App() {
 
-  //const [count, setCount] = useState(0);
+  const [jobList, updateJobs] = React.useState([]);
 
   // Similar to componentDidMount and componentDidUpdate:
   React.useEffect(() => {
     // Update the document title using the browser API
     //document.title = `You clicked ${count} times`;
-    fetchJobs();
-  });
+    fetchJobs(updateJobs);
+  }, []);
 
 
   return (
     <div className="App">
-      <Jobs jobs={mockJobs} />
+      <Jobs jobs={jobList} />
     </div>
   );
 }
