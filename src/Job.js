784a9e3e975f083a01fab2bdc00e622747ab7e9f
marciorc <mroberto@gmail.com>
2020-04-03 23:46:37 -0300

    added pagination and job modal

--- src/Job.js
+++ src/Job.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
 
-export default function Job({job}) {
+export default function Job({job, onClick}) {
     return (
-        <div className={'job'}>
-            {job.title}
-            {job.company}
+        <Paper onClick={onClick}  className="job">
+        <div>
+            <Typography variant="h6">{job.title}</Typography>
+            <Typography variant="h5">{job.company}</Typography>
+            <Typography>{job.location}</Typography>
+            
         </div>
+        <div>
+        <Typography>{job.created_at.split(' ').slice(0, 3).join(' ') }</Typography>
+        </div>
+        </Paper>
     );
 }
--- src/Jobs.js
+++ src/Jobs.js
@@ -2,18 +2,97 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 
 import Job from './Job';
+import JobModal from './JobModal';
+
+import { makeStyles } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 400,
+    flexGrow: 1,
+  },
+});
+
+const LINES_PER_PAGE = 50;
 
 export default function Jobs({jobs}) {
+    console.log('job', jobs[0]);
+
+    //modal
+    const [open, setOpen] = React.useState(false);
+    const [selectedJob, selectJob] = React.useState({});
+
+    const handleClickOpen = () => {
+        setOpen(true);
+      };
+    
+      const handleClose = () => {
+        setOpen(false);
+      };
+
+    //pagination
+    const [activeStep, setActiveStep] = React.useState(0);
+    const numJobs = jobs.length;
+    const numPages = Math.ceil(numJobs / LINES_PER_PAGE);
+
+    const actualPage = activeStep * LINES_PER_PAGE;
+
+    const jobsOnPage = jobs.slice(actualPage, actualPage * LINES_PER_PAGE + LINES_PER_PAGE);    
+    
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+
     return (
         <div className="jobs">
-            <Typography variant="h1">
+            <JobModal open={open} job={selectedJob} handleClose={handleClose} />
+            <Typography variant="h4" component="h1">
                 Entry Level Software Jobs
             </Typography>
+            <Typography variant="h6" component="h2">
+                Jobs found: {numJobs}
+            </Typography>
             {
-                jobs.map(
-                    job => <Job job={job} />
+                jobsOnPage.map(
+                    (job, i) => <Job key={i} job={job} onClick={() => {
+                            console.log('clicked');
+                            selectJob(job);
+                            handleClickOpen();
+                        }
+                    } />
                 )
             }
+            <div>
+                Page {activeStep + 1} of {numPages}
+            </div>
+            <MobileStepper
+                variant="progress"
+                steps={numPages}
+                position="static"
+                activeStep={activeStep}
+              
+                nextButton={
+                    <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+                    Next
+                    <KeyboardArrowRight />
+                    </Button>
+                }
+                backButton={
+                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                    <KeyboardArrowLeft />
+                    Back
+                    </Button>
+                }
+            />
         </div>
     )
 }
